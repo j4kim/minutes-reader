54da156deb960c6dfd3f1116286ed5ff8076c1fc
Joaquim Perez <jivkim@gmail.com>
2020-02-13 00:04:52 +0100

    show tags in nav items

--- NavItem.vue
+++ NavItem.vue
@@ -1,9 +1,17 @@
 <template>
-    <router-link :to="page.name">{{ page.title }}</router-link>
+    <div>
+        <router-link :to="page.name">
+            {{ page.title }}
+        </router-link>
+        <tags :tags="page.tags" />
+    </div>
 </template>
 
 <script>
+import Tags from "./Tags.vue"
+
 export default {
+    components: { Tags },
     props: ["page"]
 }
 </script>
