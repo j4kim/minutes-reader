1172916d21a7a8279cf3348b65baf9ae95da69c4
Joaquim Perez <jivkim@gmail.com>
2020-02-13 18:33:03 +0100

    tag styling

--- Tags.vue
+++ Tags.vue
@@ -3,7 +3,7 @@
         <small
             v-for="tag in tags"
             :key="tag.id"
-            :class="'tag-'+tag.id" 
+            :class="'tag tag-'+tag.id" 
         >
             {{ tag.name }}
         </small>
@@ -15,3 +15,23 @@ export default {
     props: ["tags"]
 }
 </script>
+
+<style>
+    .tag {
+        background: lightgrey;
+        padding: 1px 4px;
+        margin: 2px;
+        border: 1px solid white;
+        border-radius: 4px;
+        font-size: .75em;
+    }
+    .tag-0 { background: darkkhaki }
+    .tag-5 { background: palevioletred }
+    .tag-1 { background: greenyellow }
+    .tag-3 { background: aquamarine }
+    .tag-2 { background: burlywood }
+    .tag-4 { background: cadetblue }
+    .tag-7 { background: plum }
+    .tag-6 { background: palegreen }
+    .tag-8 { background: pink }
+</style>
