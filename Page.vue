01d364a347ec0d34238d70a90f2458c8f4c04bf0
Joaquim Perez <jivkim@gmail.com>
2020-02-14 22:47:15 +0100

    use mark.js to mark search elements but it does not update correctly :/

--- Page.vue
+++ Page.vue
@@ -1,21 +1,44 @@
 <template>
     <div :style="{ marginBottom: '40px' }">
-        <div v-html="page.html" />
+        <div v-html="page.html" ref="pageContent" />
         <tags :tags="page.tags" />
     </div>
 </template>
 
 <script>
+import Mark from "mark.js"
 import Tags from "./Tags.vue"
 
 export default {
     components: { Tags },
+    data() {
+        return { markInstance: undefined }
+    },
+    mounted() {
+        this.markInstance = new Mark(this.$refs.pageContent);
+    },
+    methods: {
+        mark() {
+            let search = this.$route.query.search
+            let inst = this.markInstance
+            inst.unmark({
+                done(){
+                    if (search) {
+                        inst.mark(search)
+                    }
+                }
+            });
+        }
+    },
     computed: {
         page() {
             return this.$parent.pages.find(p => {
                 return this.$route.params.page === p.name
             }) || { html: "Chargement..." }
         }
+    },
+    watch: {
+        "$route": function(){ this.mark() },
     }
 }
 </script>
